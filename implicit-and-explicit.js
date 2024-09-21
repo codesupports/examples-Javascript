70dc315d0bd5c3a7c062a5274920f2e769b94043
Raj Kumar Maheshwari <85394585+codesupports@users.noreply.github.com>
2024-09-21 11:41:29 +0530

    Create implicit-and-explicit.js

---
+++ implicit-and-explicit.js
@@ -0,0 +1,13 @@
+# implicit and explicit--------------------------------------------------
+  
+>> Below type is implicit conversion
+
+true + true > 1 + 1 = 2
+true + false > 1 + 0 = 1
+'5' + 9 = 59 
+123 + '' = '123'
+
+>> Below type is Explicit Conversion 
+
+String(123) = '123'
+Number('10') = 10 
